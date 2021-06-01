91ce4305109c179bc461690220eb2908b0cc438c
Camila <cacagrandini@gmail.com>
2021-05-31 22:17:19 -0300

    improved javascript blog.js and added class to the articles span's

--- src/scripts/blog.js
+++ src/scripts/blog.js
@@ -8,44 +8,46 @@ function aparecerBarraPesquisa(){
     document.getElementsByClassName('searchBar')[0].style.display='flex';
 }
 function pesquisar(){
-    console.log(document.getElementById('pesquisar').value);
-    if (document.getElementById('pesquisar').value=="Gestão Financeira"){
-        document.getElementById('artigo1').style.display='grid';
-        document.getElementById('artigo1').style.marginBottom='210px';
-        document.getElementById('artigo2').style.display='none';
-        document.getElementById('artigo3').style.display='none';
-        document.getElementById('artigo4').style.display='none';
-    }
-    else if (document.getElementById('pesquisar').value=="Investimentos"){
-        document.getElementById('artigo2').style.display='grid';
-        document.getElementById('artigo2').style.marginBottom='210px';
-        document.getElementById('artigo1').style.display='none';
-        document.getElementById('artigo3').style.display='none';
-        document.getElementById('artigo4').style.display='none';
-    }
-    else if (document.getElementById('pesquisar').value=="Como Poupar?"){
-        document.getElementById('artigo3').style.display='grid';
-        document.getElementById('artigo3').style.marginBottom='210px';
-        document.getElementById('artigo1').style.display='none';
-        document.getElementById('artigo2').style.display='none';
-        document.getElementById('artigo4').style.display='none';
-    }
-    else if (document.getElementById('pesquisar').value=="Fontes de Renda"){
-        document.getElementById('artigo4').style.display='grid';
-        document.getElementById('artigo4').style.marginBottom='210px';
-        document.getElementById('artigo1').style.display='none';
-        document.getElementById('artigo2').style.display='none';
-        document.getElementById('artigo3').style.display='none';
-    }
-    else if (document.getElementById('pesquisar').value=="Todos"){
-        document.getElementById('artigo1').style.display='grid';
-        document.getElementById('artigo2').style.display='grid';
-        document.getElementById('artigo3').style.display='grid';
-        document.getElementById('artigo4').style.display='grid';
-        document.getElementById('artigo1').style.marginBottom='0px';
-        document.getElementById('artigo2').style.marginBottom='0px';
-        document.getElementById('artigo3').style.marginBottom='0px';
-        document.getElementById('artigo4').style.marginBottom='0px';
-    }
+    let artigo1=document.getElementById('artigo1').style;
+        let artigo2=document.getElementById('artigo2').style;
+        let artigo3=document.getElementById('artigo3').style;
+        let artigo4=document.getElementById('artigo4').style;
+        let pesquisar=document.getElementById('pesquisar').value;
+        let span=document.getElementsByTagName('span');
+    
+        if (pesquisar=="Gestão Financeira" || pesquisar=='Gestão' || pesquisar=='gestão' || pesquisar=='gestão financeira' || pesquisar=='educação financeira'){
+            for (let i =0; i<span.length; i++){
+            span[i].style.display='none'
+        }
+            artigo1.display='grid';
+            artigo1.marginBottom='210px';
+        }
+        else if (pesquisar=="Investimentos" || pesquisar=='investimentos' || pesquisar=='investimento'){
+            for (let i =0; i<span.length; i++){
+                span[i].style.display='none'
+            }
+            artigo2.display='grid';
+            artigo2.marginBottom='210px';
+        }
+        else if (pesquisar=="Como Poupar?" || pesquisar=='Como poupar?' || pesquisar=='Como poupar' || pesquisar=='como poupar' || pesquisar=='como poupar?' || pesquisar=='poupar' || pesquisar=='economizar'){
+            for (let i =0; i<span.length; i++){
+                span[i].style.display='none'
+            }
+            artigo3.display='grid';
+            artigo3.marginBottom='210px';
+        }
+        else if (pesquisar=="Fontes de Renda" || pesquisar=='fontes de renda' || pesquisar=='renda' || pesquisar=='Renda'){
+            for (let i =0; i<span.length; i++){
+                span[i].style.display='none'
+            }
+            artigo4.display='grid';
+            artigo4.marginBottom='210px';
+        }
+        else if (pesquisar=="Todos" || pesquisar=='todos'){
+            for (let i =0; i<span.length; i++){
+                span[i].style.display='grid'
+                span[i].style.marginBottom='0px';
+            }
+        }
     document.getElementById('pesquisar').value=null;
 }
